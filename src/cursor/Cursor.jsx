c7be16ee52a5af5b2c618e5ccb845e635c633d19
Andres Vasquez <andreswd001@gmail.com>
2023-04-30 16:25:49 -0500

    Primer Commit

---
+++ src/cursor/Cursor.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from '@/styles/cursor.module.css'
+import gsap from 'gsap';
+
+function Cursor() {
+    return (
+        <div className={styles.cursor}>Cursor</div>
+    )
+}
+
+export default Cursor
