2ce9e92160c4edb6926166f823420437c5fffe8a
Andres Vasquez <andreswd001@gmail.com>
2023-04-30 17:46:53 -0500

    Mouse

--- src/cursor/Cursor.jsx
+++ src/cursor/Cursor.jsx
@@ -1,10 +1,70 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import styles from '@/styles/cursor.module.css'
 import gsap from 'gsap';
 
 function Cursor() {
+    const [cursorPos, setCursorPos] = useState({
+        x: { previous: 0, current: 0, amt: 0.2 },
+        y: { previous: 0, current: 0, amt: 0.2 }
+    });
+    const cursor = useRef(null);
+    const lerp = (a, b, n) => (1 - n) * a + n * b;
+
+    useEffect(() => {
+        const handleMouseMove = e => {
+            //Set Cursor Values
+            setCursorPos({
+                x: {
+                    previous: e.clientX,
+                    current: e.clientX,
+                    amt: 0.8
+                },
+                y: {
+                    previous: e.clientY,
+                    current: e.clientY,
+                    amt: 0.8
+                }
+            })
+            //Create animation GSAP
+            gsap.to(cursor.current, {
+                ease: "Power1.easeOut",
+                opacity: 1,
+                duration: 1
+            })
+
+            function render() {
+                setCursorPos({
+                    x: {
+                        current: e.clientX,
+                        previous: lerp(cursorPos.x.previous, cursorPos.x.current, cursorPos.x.amt)
+                    },
+                    y: {
+                        current: e.clientY,
+                        previous: lerp(cursorPos.x.previous, cursorPos.x.current, cursorPos.x.amt)
+                    }
+                })
+            }
+
+            requestAnimationFrame(() => render())
+
+        }
+
+        //Cleanup function
+        window.addEventListener('mousemove', handleMouseMove)
+        return () => {
+            window.removeEventListener(
+                'mousemove',
+                handleMouseMove,
+            );
+        }
+    }, [])
+
     return (
-        <div className={styles.cursor}>Cursor</div>
+        <div className={styles.cursor} ref={cursor} style={{
+            transform: `translateX(${cursorPos.x.previous}px) translateY(${cursorPos.y.previous}px)`
+        }}>
+            Cursor: X {cursorPos.x.current} / Y {cursorPos.y.current}
+        </div>
     )
 }
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,8 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
+// import Cursor from '@/cursor/Cursor'
+import CustomCursor from '@/cursor/CursorCustom'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -14,6 +16,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <CustomCursor />
       <main className={`${styles.main} ${inter.className}`}>
         <div className={styles.description}>
           <p>
